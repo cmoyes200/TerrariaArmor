978da05b7c1bdee72a7735d558012cf5227f4bf9
cmoyes200 <cmoyes200@caledonian.ac.uk>
2019-11-15 16:42:05 +0000

    Fixed infinite loop on reader

--- DataAccess.java
+++ DataAccess.java
@@ -3,21 +3,21 @@ import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DataAccess {
 
     public void load() {
         try {
-            File file = new File("config.txt");
+            File file = new File("C:\\Users\\Craig\\IdeaProjects\\TerrariaArmor\\src\\config.txt");
             BufferedReader in = new BufferedReader(new FileReader(file));
-            String[] stats;
 
-            String line = in.readLine();
-            while (line != null) {
-                String linearray[] = line.split("=", 2);
-                line = linearray[line.length()-1];
-                System.out.println(line);
-                stats = line.split(",", 0);
+            String line;
+            while ((line = in.readLine()) != null) {
+                ArrayList<String> linearray = new ArrayList<>(Arrays.asList(line.split("=", 2)));
+                String statsstring = linearray.get(linearray.size()-1);
+                System.out.println(statsstring);
+                ArrayList<String> stats = new ArrayList<>(Arrays.asList(statsstring.split(",", 0)));
             }
         }
 
